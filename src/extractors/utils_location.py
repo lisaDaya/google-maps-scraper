190565244967234eb6dfb80e39a57802f9c2162a
lisaDaya <lisaday8548@iejesusmirey.com>
2025-11-08 15:40:05 -0600

    feat: add src/extractors/utils_location.py

---
+++ src/extractors/utils_location.py
@@ -0,0 +1,4 @@
+thondef get_location_coordinates(address):
+    # For this example, we're just mocking the functionality
+    # In real case, this would interact with a geolocation API
+return {"latitude": 40.7128, "longitude": -74.0060}  # New York Coordinates
